1b4b2b28a1ebfce99567f2bc48e40affdf7b55ac
Александр Вязников <alexnder.vyaznikov@yandex.ru>
2023-02-01 21:41:48 +1000

    Четвертый вариант кода

--- settings.py
+++ settings.py
@@ -9,13 +9,13 @@ SHORT_LINK_LENGTH = 6
 SHORT_LINK_PATTERN = re.compile(f'^[{SHORT_LINK_SYMBOLS}]+$')
 USER_SHORT_LINK_LENGTH = 16
 URL_PATTERN = re.compile(
-        r'^(?:http|ftp)s?://'
-        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+'
-        r'(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'
-        r'localhost|'
-        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
-        r'(?::\d+)?'
-        r'(?:/?|[/?]\S+)$', re.IGNORECASE
+    r'^(?:http|ftp)s?://'
+    r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+'
+    r'(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'
+    r'localhost|'
+    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
+    r'(?::\d+)?'
+    r'(?:/?|[/?]\S+)$', re.IGNORECASE
 )
 
 
